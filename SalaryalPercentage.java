04fb6f321fbe45ac2b9b6cf300587acdd562f6f4
Caio Batista <caioxd38@outlook.com>
2023-08-31 15:53:16 -0300

    first commit

---
+++ SalaryalPercentage.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SalaryalPercentage{
+  public static void main (String[]args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your salary: ");
+       double wage = sc.nextDouble();
+      System.out.println("Enter the salary adjustment percentage:  ");
+       double result = sc.nextDouble();
+       System.out.println("This is the readjustment: " +wage*result/100 );
+
+
+
+  }
+}
